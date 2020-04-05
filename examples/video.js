cbb09c90815fd622aa5f9c2a37d2d3955dbab773
dalisoft <dalisoft@mail.ru>
2020-04-05 18:01:47 +0500

    example(video): add video example

---
+++ examples/video.js
@@ -0,0 +1,15 @@
+import nanoexpress from '../src/nanoexpress.js';
+import path from 'path';
+
+const app = nanoexpress();
+
+app.get('/video.mp4', (req, res) => {
+  res.onAborted(() => {});
+  req.headers = {};
+  req.forEach((key, value) => {
+    req.headers[key] = value;
+  });
+  res.sendFile(path.resolve('./examples/video/video.mp4'), true, false);
+});
+
+app.listen(4000);
