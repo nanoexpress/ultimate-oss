7614de8ae022a79301b820b26e29d5737dc282d9
dalisoft <dalisoft@mail.ru>
2021-03-25 22:57:27 +0500

    fix: chore build fix

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,14 @@
 const external = [
   'uWebSockets.js',
   './Route.js',
-  './App.js',
+  './app.js',
   'events',
   'http',
   'zlib',
   'fs'
 ];
 
-export default ['nanoexpress', 'Route', 'App'].map((name) => ({
+export default ['nanoexpress', 'Route', 'app'].map((name) => ({
   input: `./src/${name}.js`,
   output: {
     format: 'cjs',
